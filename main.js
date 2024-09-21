f401a4e5280ef14d750c12662b6afc14623bd990
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-09-21 14:16:24 +0300

    test between pages transition

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ window.transitionToPage = function (href) {
   document.querySelector("body").style.opacity = 0;
   setTimeout(function () {
     window.location.href = href;
-  }, 500);
+  }, 300);
 };
 
 function setupOverlayScreen(imagesObjArr) {
