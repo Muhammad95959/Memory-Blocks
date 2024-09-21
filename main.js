e7045fcadc4979f3858296778a4aa1df65c29fe5
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-09-21 07:34:13 +0300

    add overlay-screen

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+const overlayScreen = document.querySelector(".overlay-screen");
+const difficultyBtns = document.querySelectorAll(".difficulty a");
+const nameInput = document.querySelector(".player-name input");
+const startBtn = document.querySelector(".start-btn");
 const cardsBox = document.querySelector(".cards-box");
-let level = 1;
+
+let level = 2;
 let cardsCount = 20;
+let cardsCountArr = [12, 20, 30];
 let flippedCardsCount = 0;
 let wrongTries = 0;
 let duration = 1000;
@@ -8,9 +14,34 @@ let duration = 1000;
 fetch("database/images.json")
   .then((r) => r.json())
   .then((imagesObjArr) => {
+    document.body.style.visibility = "visible";
+    document.body.style.opacity = "100%";
     const randomImagesObjArr = getRandomImages(imagesObjArr);
+    setupOverlayScreen(imagesObjArr);
     setupCardsBox(randomImagesObjArr);
   });
+
+function setupOverlayScreen(imagesObjArr) {
+  [...difficultyBtns].forEach((btn, index) => {
+    btn.onclick = () => {
+      [...difficultyBtns].forEach((b) => b.classList.remove("selected"));
+      btn.classList.add("selected");
+      cardsBox.innerHTML = "";
+      level = index + 1;
+      cardsCount = cardsCountArr[level - 1];
+      const randomImagesObjArr = getRandomImages(imagesObjArr);
+      setupCardsBox(randomImagesObjArr);
+    };
+  });
+  startBtn.onclick = () => {
+    let name = nameInput.value;
+    if (!(name === null || name === ""))
+      document.querySelector(".top-info .name").textContent = name;
+    overlayScreen.style.transition = "opacity 0.5s";
+    overlayScreen.style.opacity = "0";
+    setTimeout(() => overlayScreen.remove(), 500);
+  };
+}
 
 function getRandomImages(imagesObjArr) {
   let randomImagesObjArr = new Array(cardsCount);
@@ -34,6 +65,8 @@ function setupCardsBox(randomImagesObjArr) {
   [...randomImagesObjArr].forEach((imageObj, index) => {
     const card = document.createElement("div");
     cardsBox.appendChild(card);
+    cardsBox.classList = "";
+    cardsBox.classList = `cards-box level-${level}`;
     card.classList.add(`card-${index + 1}`);
     card.setAttribute("image-id", imageObj.id);
     card.onclick = () => setupCardFlip(card);
@@ -65,8 +98,12 @@ function setupCardFlip(card) {
       });
     } else {
       increaseWrongTries();
+      flippedCards.forEach((c) => {
+        c.classList.remove("is-flipped");
+        c.classList.add("is-unflipping");
+      });
       setTimeout(() => {
-        flippedCards.forEach((c) => c.classList.remove("is-flipped"));
+        flippedCards.forEach((c) => c.classList.remove("is-unflipping"));
       }, duration);
     }
     flippedCardsCount = 0;
